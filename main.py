99fd9b0a88dd57f5b5829aa30711c21653a8dc84
oddangryshot <lists@oddangryshot.com>
2026-03-09 03:16:37 +1000

    H: Added system prompts and function declaration

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from prompts import system_prompt
+from call_functions import available_functions
 
 def main():
     # 1. Load configuration from .env
@@ -21,7 +23,6 @@ def main():
     parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
 
-
     # 3. Initialize the Gemini client
     client = genai.Client(api_key=api_key)
 
@@ -36,7 +37,12 @@ def main():
     # 5. Generate content
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents=args.user_prompt,
+        contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions],
+            system_instruction=system_prompt,
+            temperature=0
+        ),
     )
 
     # 6: Check usage_metadata and print token counts
@@ -45,13 +51,17 @@ def main():
     if usage_metadata is None:
         raise RuntimeError("No usage_metadata returned. This likely indicates a failed API request. Check your API key, quota, or try again later.")
 
-    if args.verbose:
-        print(f"User prompt: {args.user_prompt}")
-        print(f"Prompt tokens: {usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {usage_metadata.candidates_token_count}")
-
-    print(response.text)
-
+    # 7: Handle response: function calls OR text
+    function_calls = response.function_calls
+    if function_calls:
+        for function_call in function_calls:
+            print(f"Calling function: {function_call.name}({function_call.args})")
+    else:
+        if args.verbose:
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {usage_metadata.candidates_token_count}")
+        print(response.text)
 
 if __name__ == "__main__":
     main()
